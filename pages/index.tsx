45ce2d5b67780a3f826f6007bc2c2a207fd644a4
DevinParentice <dude114100@gmail.com>
2021-04-10 01:08:20 -0400

    Added dynamic select options

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,60 @@
 import Head from "next/head";
-import Option1 from "../components/Option1";
+import React, { useState } from "react";
 
 export default function Home() {
+	const selectNames = ["select1", "select2"];
+	const [options] = useState([
+		{
+			name: "directed by",
+			value: "directed-by",
+		},
+		{
+			name: "starring",
+			value: "starring",
+		},
+		{
+			name: "with the genre",
+			value: "genre",
+		},
+		{
+			name: "released in the year",
+			value: "release-year",
+		},
+		{
+			name: "released before the year",
+			value: "release-year-before",
+		},
+		{
+			name: "released after the year",
+			value: "release-year-after",
+		},
+		{
+			name: "with a rating higher than",
+			value: "rating-higher",
+		},
+		{
+			name: "with a rating lower than",
+			value: "rating-lower",
+		},
+	]);
+
+	const [chosenOptions, setChosenOptions] = useState({});
+
+	const isChosenByOther = (optionValue: string, selectName: string) => {
+		for (let key in chosenOptions) {
+			if (key !== selectName) {
+				if (chosenOptions[key] === optionValue) {
+					return true;
+				}
+			}
+		}
+		return false;
+	};
+
+	const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setChosenOptions({ ...chosenOptions, [e.target.name]: e.target.value });
+	};
+
 	return (
 		<div>
 			<Head>
@@ -17,7 +70,35 @@ export default function Home() {
 				</select>
 				<p className="movies-text">&nbsp;movie(s)</p>
 				<br />
-				<Option1 />
+				{selectNames.map((name, index) => {
+					return (
+						<div>
+							<select
+								name={name}
+								key={index}
+								onChange={handleChange}
+								value={chosenOptions[name] || ""}
+								required={index === 0}
+							>
+								<option value="" disabled={chosenOptions[name]}>
+									Choose a parameter...
+								</option>
+								{options
+									.filter(({ value }) => !isChosenByOther(value, name))
+									.map(({ name, value }, oIndex) => (
+										<option value={value} key={oIndex}>
+											{name}
+										</option>
+									))}
+							</select>
+							<input
+								type="text"
+								required={index === 0}
+								name={"input-" + ++index}
+							/>
+						</div>
+					);
+				})}
 				<br />
 				<input type="submit" value="Search" />
 			</form>
