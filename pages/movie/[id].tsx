800f529cfa433c1061a718b8c2d4af60e075de49
DevinParentice <dude114100@gmail.com>
2021-04-12 19:16:00 -0400

    Added list of cast and crew to display on movie page

--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -13,7 +13,8 @@ class MoviePage extends React.Component<MyComponentProps, any> {
 
 		this.state = {
 			movie: {},
-			cast: {},
+			people: {},
+			peopleDisplay: {},
 			loading: true,
 		};
 	}
@@ -34,7 +35,7 @@ class MoviePage extends React.Component<MyComponentProps, any> {
 		const url = `https://api.themoviedb.org/3/movie/${this.props.router.query.id}/credits?api_key=${process.env.API_KEY}`;
 		const res = await fetch(url);
 		const data = await res.json();
-		this.setState({ cast: data, loading: false });
+		this.setState({ people: data, peopleDisplay: data.cast, loading: false });
 	}
 
 	formatGenres() {
@@ -64,35 +65,77 @@ class MoviePage extends React.Component<MyComponentProps, any> {
 
 		return (
 			<div>
-				{this.state.movie.poster_path ? (
-					<img
-						src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${this.state.movie.poster_path}`}
-						alt={`${this.state.movie.title} Poster`}
-						className="movie-poster-lg"
-					/>
-				) : (
-					<Image
-						src="/NoPoster.png"
-						alt="No Poster Found"
-						width="220"
-						height="330"
-						className="movie-poster-lg"
-					/>
-				)}
-				<h1>{this.state.movie.title}</h1>
-				<h2>Released {this.formatDate()}</h2>
-				{this.state.cast.crew.map((castMember) => {
-					if (castMember.job === "Director") {
-						return (
-							<h2 key={castMember.credit_id}>Directed by {castMember.name}</h2>
-						);
-					}
-				})}
-				<div>
-					<h3>{this.formatGenres()}</h3>
+				<div className="movie-details">
+					{this.state.movie.poster_path ? (
+						<img
+							src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${this.state.movie.poster_path}`}
+							alt={`${this.state.movie.title} Poster`}
+							className="movie-poster-lg"
+						/>
+					) : (
+						<Image
+							src="/NoPoster.png"
+							alt="No Poster Found"
+							width="220"
+							height="330"
+							className="movie-poster-lg"
+						/>
+					)}
+					<div>
+						<h1>{this.state.movie.title}</h1>
+						<h2>Released {this.formatDate()}</h2>
+						{this.state.people.crew.map((castMember) => {
+							if (castMember.job === "Director") {
+								return (
+									<h2 key={castMember.credit_id}>
+										Directed by {castMember.name}
+									</h2>
+								);
+							}
+						})}
+						<div>
+							<h3>{this.formatGenres()}</h3>
+						</div>
+						<h4>{this.state.movie.tagline}</h4>
+						<p>{this.state.movie.overview}</p>
+					</div>
 				</div>
-				<h4>{this.state.movie.tagline}</h4>
-				<p>{this.state.movie.overview}</p>
+
+				<div>
+					<ul className="content-selector">
+						<li
+							onClick={() => {
+								this.setState({ peopleDisplay: this.state.people.cast });
+							}}
+						>
+							Cast
+						</li>
+						<li
+							onClick={() => {
+								this.setState({ peopleDisplay: this.state.people.crew });
+							}}
+						>
+							Crew
+						</li>
+					</ul>
+				</div>
+				<div>
+					{this.state.peopleDisplay.map((person) => {
+						if (person.character) {
+							return (
+								<p>
+									{person.name} - {person.character}
+								</p>
+							);
+						} else {
+							return (
+								<p>
+									{person.name} - {person.job}
+								</p>
+							);
+						}
+					})}
+				</div>
 			</div>
 		);
 	}
