a4eb82d02904d785310c468a79e2231e1f3040ab
DevinParentice <dude114100@gmail.com>
2021-04-14 00:21:17 -0400

    Started search form overhaul

--- components/Options.tsx
+++ components/Options.tsx
@@ -1,16 +1,10 @@
 import React, { useState } from "react";
 
 export default function Options() {
-	const selectNames = [
-		"select0",
-		"select1",
-		"select2",
-		"select3",
-		"select4",
-		"select5",
-		"select6",
-		"select7",
-	];
+	const [selects, setSelects] = useState(["select0"]);
+	const [inputs, setInputs] = useState([]);
+	const [chosenOptions, setChosenOptions] = useState({});
+
 	const [options] = useState([
 		{
 			name: "directed by",
@@ -37,18 +31,112 @@ export default function Options() {
 			value: "release-year-after",
 		},
 		{
-			name: "with a rating greater than (1-10)",
+			name: "with a rating greater than",
 			value: "rating-higher",
 		},
 		{
-			name: "with a rating less than (1-10)",
+			name: "with a rating less than",
 			value: "rating-lower",
 		},
 	]);
 
-	const [chosenOptions, setChosenOptions] = useState<object>({});
+	const handleChange = (e) => {
+		setChosenOptions({ ...chosenOptions, [e.target.name]: e.target.value });
+		if (!selects[parseInt(e.target.name.slice(-1)) + 1]) {
+			setSelects(selects.concat(`select${selects.length}`));
+		}
+		let arr = [...inputs];
+		arr[parseInt(e.target.name.slice(-1))] = e.target.value;
+		setInputs(arr);
+	};
 
-	const isChosenByOther = (optionValue: string, selectName: string) => {
+	const getInput = (index: number) => {
+		if (inputs[index] === "directed-by") {
+			return (
+				<>
+					<input
+						type="text"
+						name={`input${index}`}
+						required={index === 0}
+						placeholder="Enter full name"
+					/>
+					<p className="movies-text">,</p>
+				</>
+			);
+		} else if (inputs[index] === "starring") {
+			return (
+				<>
+					<input
+						type="text"
+						name={`input${index}`}
+						required={index === 0}
+						placeholder="Comma separated names"
+					/>
+					<p className="movies-text">,</p>
+				</>
+			);
+		} else if (inputs[index] === "genres") {
+			return (
+				<>
+					<select name={`input${index}`} id="genre-select">
+						<option value="action">Action</option>
+						<option value="adventure">Adventure</option>
+						<option value="animation">Animation</option>
+						<option value="comedy">Comedy</option>
+						<option value="crime">Crime</option>
+						<option value="documentary">Documentary</option>
+						<option value="drama">Drama</option>
+						<option value="family">Family</option>
+						<option value="fantasy">Fantasy</option>
+						<option value="history">History</option>
+						<option value="horror">Horror</option>
+						<option value="music">Music</option>
+						<option value="mystery">Mystery</option>
+						<option value="romance">Romance</option>
+						<option value="science-fiction">Science Fiction</option>
+						<option value="tv-movie">TV Movie</option>
+						<option value="thriller">Thriller</option>
+						<option value="war">War</option>
+						<option value="western">Western</option>
+					</select>
+					<p className="movies-text">,</p>
+				</>
+			);
+		} else if (
+			inputs[index] === "release-year" ||
+			inputs[index] === "release-year-before" ||
+			inputs[index] === "release-year-after"
+		) {
+			return (
+				<>
+					<input
+						type="text"
+						name={`input${index}`}
+						required={index === 0}
+						placeholder="Enter year"
+					/>
+					<p className="movies-text">,</p>
+				</>
+			);
+		} else if (
+			inputs[index] === "rating-higher" ||
+			inputs[index] === "rating-lower"
+		) {
+			return (
+				<>
+					<input
+						type="text"
+						name={`input${index}`}
+						required={index === 0}
+						placeholder="0.0 - 10.0"
+					/>
+					<p className="movies-text">,</p>
+				</>
+			);
+		}
+	};
+
+	const isChosenByOther = (optionValue, selectName) => {
 		for (let key in chosenOptions) {
 			if (key !== selectName) {
 				if (chosenOptions[key] === optionValue) {
@@ -61,48 +149,26 @@ export default function Options() {
 
 	return (
 		<div>
-			{selectNames.map((name, index) => {
+			{selects.map((select, index) => {
 				return (
-					<div
-						id={`parameter-${index}`}
-						key={`parameter-${index}`}
-						hidden={index !== 0}
-					>
+					<div key={index}>
 						<select
-							name={name}
+							name={select}
 							id={`select-${index}`}
-							onChange={(e) => {
-								setChosenOptions({
-									...chosenOptions,
-									[e.target.name]: e.target.value,
-								});
-								if (document.getElementById(`parameter-${index + 1}`)) {
-									document
-										.getElementById(`parameter-${index + 1}`)
-										.removeAttribute("hidden");
-								}
-							}}
-							value={chosenOptions[name] || ""}
+							onChange={handleChange}
 							required={index === 0}
+							value={chosenOptions[select] || ""}
 						>
-							<option value="" disabled={chosenOptions[name]}>
-								Choose a parameter...
-							</option>
+							<option value="">Choose a parameter...</option>
 							{options
-								.filter(({ value }) => !isChosenByOther(value, name))
+								.filter(({ value }) => !isChosenByOther(value, select))
 								.map(({ name, value }, oIndex) => (
 									<option value={value} key={oIndex}>
 										{name}
 									</option>
 								))}
 						</select>
-						<input
-							name={`input${index}`}
-							type="text"
-							required={index === 0}
-							className="search-options"
-						/>
-						<p className="movies-text">,</p>
+						{getInput(index)}
 					</div>
 				);
 			})}
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,31 +8,12 @@ export default function Home() {
 
 	const submitForm = async (e) => {
 		e.preventDefault();
-		const formData = {
-			[e.target.select0.value]: e.target.input0.value,
-			...(e.target.select1.value && {
-				[e.target.select1.value]: e.target.input1.value,
-			}),
-			...(e.target.select2.value && {
-				[e.target.select2.value]: e.target.input2.value,
-			}),
-			...(e.target.select3.value && {
-				[e.target.select3.value]: e.target.input3.value,
-			}),
-			...(e.target.select4.value && {
-				[e.target.select4.value]: e.target.input4.value,
-			}),
-			...(e.target.select5.value && {
-				[e.target.select5.value]: e.target.input5.value,
-			}),
-			...(e.target.select6.value && {
-				[e.target.select6.value]: e.target.input6.value,
-			}),
-			...(e.target.select7.value && {
-				[e.target.select7.value]: e.target.input7.value,
-			}),
-		};
-
+		const arr = [...e.target.elements].slice(0, -2);
+		let formData = {};
+		for (let i = 0; i < arr.length; i += 2) {
+			console.log(`${arr[i].value} ---> ${arr[i + 1].value}`);
+			formData[arr[i].value] = arr[i + 1].value;
+		}
 		const res = await fetch("/api/search", {
 			body: JSON.stringify(formData),
 			headers: {
