a3d23c332c90f2298a45528200ce74ccfa4d6547
DevinParentice <dude114100@gmail.com>
2021-04-20 16:17:23 -0400

    Fixed bug with displaying dates

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,10 +1,10 @@
 export default function formatDate(date: string): string {
 	if (date !== "") {
 		let finalDate = "";
-
+		date = date.replaceAll("-", "/");
 		const newDate = new Date(date);
 		const month = newDate.toLocaleString("default", { month: "long" });
-		finalDate = `${month} ${newDate.getDay()}, ${newDate.getFullYear()}`;
+		finalDate = `${month} ${newDate.getDate()}, ${newDate.getFullYear()}`;
 		return finalDate;
 	}
 	return "Not yet released";
